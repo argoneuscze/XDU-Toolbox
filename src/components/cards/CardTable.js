ac6596ccef3231c5616e05098a8610b8713b5f67
argoneus <argoneuscze@gmail.com>
2020-02-28 16:47:00 +0100

    basic table

--- src/components/cards/CardTable.js
+++ src/components/cards/CardTable.js
@@ -1,7 +1,44 @@
 import React from "react";
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+  makeStyles
+} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650
+  }
+});
 
 const CardTable = () => {
-  return "TODO table";
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>head1</TableCell>
+            <TableCell>head2</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TableRow>
+            <TableCell component="th" scope="row">
+              th
+            </TableCell>
+            <TableCell>body</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 export default CardTable;
